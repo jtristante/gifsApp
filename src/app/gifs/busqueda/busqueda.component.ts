6febcf905758d4ff6f55ddc06a91980c53d24bcf
ngonzalo <javier.tristante@gmail.com>
2021-06-18 00:28:41 +0200

    Custom Search History

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -14,7 +14,12 @@ export class BusquedaComponent {
   constructor( private gifsService: GifsService){ }
 
 buscar(): void{
+
   const valor= this.txtBuscar.nativeElement.value;
+  
+  if(valor.trim().length ===0){
+    return;
+  }
   this.gifsService.buscarGifs(valor);
   this.txtBuscar.nativeElement.value='';
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,14 @@ export class GifsService {
   }
 
   buscarGifs( query: string) {
-    this._historial.unshift( query );
+    
+    query=query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+   
     console.log(this._historial);
   }
 
